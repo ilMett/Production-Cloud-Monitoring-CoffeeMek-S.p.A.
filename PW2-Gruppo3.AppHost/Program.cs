b84d9279ef2c61170dd1471249bc90e1f421ee66
ilMett <152792531+ilMett@users.noreply.github.com>
2025-06-11 15:31:38 +0200

    fix creazione db

--- PW2-Gruppo3.AppHost/Program.cs
+++ PW2-Gruppo3.AppHost/Program.cs
@@ -4,12 +4,13 @@ var cache = builder.AddRedis("cache");
 
 var azureSql = builder.AddConnectionString("db");
 
-var apiService = builder.AddProject<Projects.PW2_Gruppo3_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.PW2_Gruppo3_ApiService>("apiservice")    
+    .WithReference(azureSql)
+    .WaitFor(azureSql);
 
 builder.AddProject<Projects.PW2_Gruppo3_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
-    .WithReference(azureSql)
     .WaitFor(cache)
     .WithReference(apiService)
     .WaitFor(apiService);
