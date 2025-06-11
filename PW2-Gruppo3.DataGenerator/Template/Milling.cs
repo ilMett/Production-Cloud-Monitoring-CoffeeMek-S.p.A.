1abc1dc34f4e3b1ccdc54cf3cef469cb40f365f6
Luca Bernardis <luca.bernardis20@gmail.com>
2025-06-11 11:50:07 +0200

    Added Json Test Classes

---
+++ PW2-Gruppo3.DataGenerator/Template/Milling.cs
@@ -0,0 +1,16 @@
+namespace PW2_Gruppo3.DataGenerator.Template;
+
+public class Milling
+{
+    public string Machine { get; set; }
+    public string CycleTime { get; set; }
+    public string CuttingDepth { get; set; }
+    public string Vibration { get; set; }
+    public string UserAlerts { get; set; }
+    public string Site { get; set; }
+    public string TimeStampLocal { get; set; }
+    public string TimeStampUtc { get; set; }
+    public string MachineBlockage { get; set; }
+    public string BlockageCause { get; set; }
+    public string LastMaintenance { get; set; }
+}
---
+++ PW2-Gruppo3.DataGenerator/Template/TestLine.cs
@@ -0,0 +1,16 @@
+namespace PW2_Gruppo3.DataGenerator.Template;
+
+public class TestLine
+{
+    public string Machine { get; set; }
+    public string TestResult { get; set; }
+    public string BoilerPressure { get; set; }
+    public string BoilerTemperature { get; set; }
+    public string EnergyConsumption { get; set; }
+    public string Site { get; set; }
+    public string TimeStampLocal { get; set; }
+    public string TimeStampUtc { get; set; }
+    public string MachineBlockage { get; set; }
+    public string BlockageCause { get; set; }
+    public string LastMaintenance { get; set; }
+}
