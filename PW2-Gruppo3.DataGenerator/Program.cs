aec96a42975d613c24b804b4115ecb3cdc0a806f
Luca Bernardis <luca.bernardis20@gmail.com>
2025-06-11 12:52:00 +0200

    added json-mock message

--- PW2-Gruppo3.DataGenerator/Program.cs
+++ PW2-Gruppo3.DataGenerator/Program.cs
@@ -63,12 +63,12 @@ namespace Pw2_Gruppo3.DataGenerator;
             
             var milling = new Milling()
             {
-                Machine = faker.PickRandom(new[] { "Fresa CNC", "Tornio Verticale", "Centro di Lavoro Orizzontale", "Macchina a 5 Assi" }),
+                Machine = "Fresa CNC",
                 CycleTime = faker.Random.Decimal(10, 60).ToString("0.00"), 
                 CuttingDepth = faker.Random.Decimal(1, 10).ToString("0.00"), 
                 Vibration = faker.Random.Decimal(0.1m, 1.5m).ToString("0.000"), 
                 UserAlerts = faker.PickRandom(new[] { "Nessun avviso", "Rottura utensile", "Sovratemperatura motore", "Mancanza di lubrificante", "Errore di programmazione" }),
-                Site = faker.Address.Country(),
+                Site = faker.PickRandom(new[] {"Italia", "Vietnam", "Brasile"}),
                 TimeStampLocal = faker.Date.Recent(5).ToString("yyyy-MM-ddTHH:mm:ss"), 
                 TimeStampUtc = faker.Date.Recent(5).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss"), 
                 MachineBlockage = isBlocked.ToString(), 
@@ -78,44 +78,44 @@ namespace Pw2_Gruppo3.DataGenerator;
 
             var lathe = new Lathe()
             {
-                Machine = faker.PickRandom(new[] { "Tornio automatico", "Tornio CNC", "Tornio Parallelo", "Tornio Verticale" }),
+                Machine = "Tornio",
                 MachineState = faker.PickRandom(new[] { "Operativa", "Inattiva", "Manutenzione", "Guasto" }),
-                RotationSpeed = faker.Random.Int(1000, 3000).ToString(), // Esempio: 2094
+                RotationSpeed = faker.Random.Int(1000, 3000).ToString(), 
                 SpindleTemperature = faker.Random.Decimal(30.0m, 60.0m).ToString("0.00"), // Esempio: 35.71
-                CompletedItems = faker.Random.Int(10, 100).ToString(), // Esempio: 57
-                Site = faker.Address.Country(),
+                CompletedItems = faker.Random.Int(10, 100).ToString(), 
+                Site = faker.PickRandom(new[] {"Italia", "Vietnam", "Brasile"}),
                 TimeStampLocal = faker.Date.Recent(5).ToString("yyyy-MM-ddTHH:mm:ss"),
                 TimeStampUtc = faker.Date.Recent(5).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss"),
-                MachineBlockage = isBlocked.ToString(), // Applica la stessa logica di blocco, convertendo in stringa
+                MachineBlockage = isBlocked.ToString(), 
                 BlockageCause = isBlocked ? faker.PickRandom(new[] { "Surriscaldamento", "Rottura utensile", "Problema elettrico", "Mancanza materiale", "Pressione idraulica bassa" }) : null, // BlockageCause è null se non bloccato
                 LastMaintenance = faker.Date.Past(2).ToString("yyyy-MM-ddTHH:mm:ss")
             };
 
             var assemblyLine = new AssemblyLine()
             {
-                Machine = faker.PickRandom(new[] { "Linea Assemblaggio Robotizzata", "Linea Assemblaggio Semiautomatica", "Linea di Finitura", "Linea di Imballaggio" }),
-                AverageStationTime = faker.Random.Decimal(15.0m, 45.0m).ToString("0.00"), // Esempio: 27.22
-                OperatorsNumber = faker.Random.Int(1, 5).ToString(), // Esempio: 1
+                Machine = "Linea di Assemblaggio",
+                AverageStationTime = faker.Random.Decimal(15.0m, 45.0m).ToString("0.00"),
+                OperatorsNumber = faker.Random.Int(1, 5).ToString(), 
                 Faults = faker.PickRandom(new[] { "Nessun difetto", "Componente mancante", "Errore di montaggio", "Difetto materiale", "Danno estetico" }),
-                Site = faker.Address.Country(),
+                Site = faker.PickRandom(new[] {"Italia", "Vietnam", "Brasile"}),
                 TimeStampLocal = faker.Date.Recent(5).ToString("yyyy-MM-ddTHH:mm:ss"),
                 TimeStampUtc = faker.Date.Recent(5).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss"),
-                MachineBlockage = isBlocked.ToString(), // Applica la stessa logica di blocco, convertendo in stringa
+                MachineBlockage = isBlocked.ToString(), 
                 BlockageCause = isBlocked ? faker.PickRandom(new[] { "Errore software", "Guasto meccanico", "Interruzione alimentazione", "Mancanza componenti", "Problema di sicurezza" }) : null, // BlockageCause è null se non bloccato
                 LastMaintenance = faker.Date.Past(2).ToString("yyyy-MM-ddTHH:mm:ss")
             };
 
             var testLine = new TestLine()
             {
-                Machine = faker.PickRandom(new[] { "Linea di Test Funzionale", "Linea di Test di Stress", "Sistema di Ispezione Ottica", "Banco di Collaudo" }),
-                TestResult = faker.PickRandom(new[] { "OK", "FAIL", "PASS" }), // Esempio: OK/FAIL
-                BoilerPressure = faker.Random.Decimal(1.0m, 5.0m).ToString("0.00"), // Esempio: 3.39
-                BoilerTemperature = faker.Random.Decimal(80.0m, 110.0m).ToString("0.00"), // Esempio: 95.47
-                EnergyConsumption = faker.Random.Decimal(1.0m, 10.0m).ToString("0.00"), // Esempio: 3.26
-                Site = faker.Address.Country(),
+                Machine = "Linea di Test",
+                TestResult = faker.PickRandom(new[] { "OK", "FAIL", "PASS" }),
+                BoilerPressure = faker.Random.Decimal(1.0m, 5.0m).ToString("0.00"), 
+                BoilerTemperature = faker.Random.Decimal(80.0m, 110.0m).ToString("0.00"), 
+                EnergyConsumption = faker.Random.Decimal(1.0m, 10.0m).ToString("0.00"),
+                Site = faker.PickRandom(new[] {"Italia", "Vietnam", "Brasile"}),
                 TimeStampLocal = faker.Date.Recent(5).ToString("yyyy-MM-ddTHH:mm:ss"),
                 TimeStampUtc = faker.Date.Recent(5).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss"),
-                MachineBlockage = isBlocked.ToString(), // Applica la stessa logica di blocco, convertendo in stringa
+                MachineBlockage = isBlocked.ToString(),
                 BlockageCause = isBlocked ? faker.PickRandom(new[] { "Guasto meccanico", "Errore del sensore", "Mancanza di fluido", "Problema software", "Calibrazione errata" }) : null, // BlockageCause è null se non bloccato
                 LastMaintenance = faker.Date.Past(2).ToString("yyyy-MM-ddTHH:mm:ss")
             };
