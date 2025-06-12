6536559e69bd68347fad7edb1b28ac4f100414da
ilMett <152792531+ilMett@users.noreply.github.com>
2025-06-12 21:27:05 +0200

    Added generic service and CRUD endpoints for Site entity (Service NEED test)

--- PW2-Gruppo3.ApiService/Program.cs
+++ PW2-Gruppo3.ApiService/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry;
 using PW2_Gruppo3.ApiService;
+using PW2_Gruppo3.ApiService.Crud;
 using PW2_Gruppo3.ApiService.Data;
+using PW2_Gruppo3.ApiService.Services;
+using PW2_Gruppo3.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,16 @@ builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register API services
+builder.Services.AddScoped<IGenericService<Batch>, GenericService<Batch>>();
+builder.Services.AddScoped<IGenericService<Customer>, GenericService<Customer>>();
+builder.Services.AddScoped<IGenericService<Site>, GenericService<Site>>();
+builder.Services.AddScoped<IGenericService<AssemblyLine>, GenericService<AssemblyLine>>();
+builder.Services.AddScoped<IGenericService<Milling>, GenericService<Milling>>();
+builder.Services.AddScoped<IGenericService<TestLine>, GenericService<TestLine>>();
+builder.Services.AddScoped<IGenericService<Lathe>, GenericService<Lathe>>();
+
 
 builder.Services.AddDbContext<ProductionMonitoringContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("db")));
@@ -31,6 +45,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// Map of all EndPoints
+app.MapSiteEndPoint();
 app.MapDefaultEndpoints();
 app.MapDataGeneratorEndPoint();
 
